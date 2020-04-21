eb1a0fd36fff2e330b70d5c8e6d2580b47c3ce0d
unknown <tanmayrsm@gmail.com>
2020-04-22 03:15:54 +0530

    first

---
+++ 1.py
@@ -0,0 +1,131 @@
+import cv2, numpy as np
+import utils
+
+########
+path = "1.jpg"
+imgWidth = 300
+imgHeight = 400
+questions = 5
+choices = 5
+ans = [1,2,0,1,4]
+########
+
+pic = cv2.imread(path)
+pic = cv2.resize(pic,(imgWidth, imgHeight))
+
+picContours = pic.copy()
+picBiggestContours = pic.copy()
+picFinal = pic.copy()
+
+
+picGray = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
+picBlur = cv2.GaussianBlur(picGray , (5,5), 1)
+picCanny = cv2.Canny(picBlur, 10, 50)
+
+
+## finding all contours
+contours, heirarchy = cv2.findContours(picCanny, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+cv2.drawContours(picContours, contours, -1, (0,255,0), 1)
+
+# find rectangles
+rectCon = utils.rectContour(contours)
+biggestContour = utils.getCornerPoints(rectCon[0]) 				#get four co-ord of biggest rect
+gradePoints = utils.getCornerPoints(rectCon[1])
+
+if biggestContour.size != 0 and gradePoints.size != 0:
+	cv2.drawContours(picBiggestContours, biggestContour, -1, (255,0,0), 7)
+	cv2.drawContours(picBiggestContours, gradePoints, -1, (0,0,255), 7)
+	#print("big cont:",biggestContour.shape)	--> (4,1,2)
+	biggestContour = utils.reorderPoints(biggestContour)
+	gradePoints    = utils.reorderPoints(gradePoints)
+
+	# get bird eye view of biggest contour
+	pt1 = np.float32(biggestContour)
+	pt2 = np.float32([ [0,0],[imgWidth,0],[0,imgHeight],[imgWidth, imgHeight] ])
+	matrix = cv2.getPerspectiveTransform(pt1, pt2)
+	imgWarpColored = cv2.warpPerspective(pic , matrix , (imgWidth, imgHeight))
+
+	#get bird view of second biggest
+	ptG1 = np.float32(gradePoints)
+	ptG2 = np.float32([ [0,0],[325,0],[0,150],[325, 150] ])
+	matrixG = cv2.getPerspectiveTransform(ptG1, ptG2)
+	imgWarpGrade = cv2.warpPerspective(pic , matrixG , (325, 150))
+	#cv2.imshow("grade",imgWarpGrade)
+
+	#apply threshold in biggest contour to get the grades marked
+	imgWarpGray = cv2.cvtColor(imgWarpColored, cv2.COLOR_BGR2GRAY)
+	imgThresh   = cv2.threshold(imgWarpGray, 170,255,cv2.THRESH_BINARY_INV)[1]
+
+	#get each bubble as separate image re
+	boxes = utils.splitBoxes(imgThresh)
+	#print(cv2.countNonZero(boxes[1]) , cv2.countNonZero(boxes[2]))
+	#cv2.imshow("test",boxes[2])
+
+	#GET NON ZERO PIXEL VALUE OF EACH BOX
+	myPixelVal = np.zeros((questions, choices))
+	countC = 0
+	countR = 0
+
+	for image in boxes:
+		totalPixels = cv2.countNonZero(image)
+		myPixelVal[countR][countC] = totalPixels
+		countC += 1
+		if (countC == choices):	
+			countR += 1
+			countC = 0
+
+	myIndex = []
+	for x in range(questions):
+		arr = myPixelVal[x]
+		myIndexVal = np.where(arr == np.amax(arr))
+		myIndex.append(myIndexVal[0][0])
+	#ans marked -->
+	#print(myIndex)
+
+	#GRADE THE ANSWERS
+	grading = []
+	for x in range(questions):
+		if ans[x] == myIndex[x]:
+			grading.append(1)
+		else:	grading.append(0)
+
+	#	SCORING CALCULATION
+	score = (sum(grading)/questions) *100
+	print(score)
+
+	#	DISPLAYING ANSWERS
+	imgResult = imgWarpColored.copy()
+	imgResult = utils.showAnswers(imgResult, myIndex, grading, ans, questions, choices)
+	imRawDrawing = np.zeros_like(imgWarpColored)
+	imRawDrawing = utils.showAnswers(imRawDrawing, myIndex, grading, ans, questions, choices)
+
+	#	INVERSE PERS --> BIRD VIEW SE ORIGNINAL IMAGE TAK...MEANS POINTS LE GREEN AND RED AND MARK ON MAIN SCREEN
+	invMatrix = cv2.getPerspectiveTransform(pt2, pt1)
+	imgInverseWarp = cv2.warpPerspective(imRawDrawing , invMatrix , (imgWidth, imgHeight))
+
+	#	GET GRADE IN THE BOX
+	imgRawGrade = np.zeros_like(imgWarpGrade)
+	cv2.putText(imgRawGrade, str(int(score)) + "%", (60, 100),cv2.FONT_HERSHEY_COMPLEX, 3, (255,255,255),3)
+	#cv2.imshow("grade",imgRawGrade)
+
+	#	INVERSE GRADE PERSPECTIVE 
+	invMatrixG = cv2.getPerspectiveTransform(ptG2, ptG1)
+	imgInvGrade = cv2.warpPerspective(imgRawGrade , invMatrixG , (imgWidth, imgHeight))
+
+
+	#	FINAL IMAGE
+	picFinal = cv2.addWeighted(picFinal, 1, imgInverseWarp, 1, 0)
+	picFinal = cv2.addWeighted(picFinal, 1, imgInvGrade, 1, 0)
+
+lables = [["Original","Gray","Edges","Canny"],
+              ["Contour","biggest contour","Warpped","Threshold"],
+              ["Result","Raw drawing","Inv Warp","Final"]]
+
+imgBlank = np.zeros_like(pic)
+imgArray = ([pic,picGray,picBlur,picCanny],[picContours, picBiggestContours, imgWarpColored, imgThresh],[imgResult, imRawDrawing, imgInverseWarp, picFinal])
+imgStack = utils.stackImages(imgArray, 0.5, lables)
+
+cv2.imshow("final",picFinal)
+
+cv2.imshow("pakode",imgStack)
+cv2.waitKey(0)
---
+++ utils.py
@@ -0,0 +1,118 @@
+import cv2 , numpy as np
+
+def stackImages(imgArray,scale,lables=[]):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+            hor_con[x] = np.concatenate(imgArray[x])
+        ver = np.vstack(hor)
+        ver_con = np.concatenate(hor)
+    else:
+        for x in range(0, rows):
+            imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        hor_con= np.concatenate(imgArray)
+        ver = hor
+    if len(lables) != 0:
+        eachImgWidth= int(ver.shape[1] / cols)
+        eachImgHeight = int(ver.shape[0] / rows)
+        #print(eachImgHeight)
+        for d in range(0, rows):
+            for c in range (0,cols):
+                cv2.rectangle(ver,(c*eachImgWidth,eachImgHeight*d),(c*eachImgWidth+len(lables[d][c])*13+27,30+eachImgHeight*d),(255,255,255),cv2.FILLED)
+                cv2.putText(ver,lables[d][c],(eachImgWidth*c+10,eachImgHeight*d+20),cv2.FONT_HERSHEY_COMPLEX,0.7,(255,0,255),2)
+    return ver
+
+
+def rectContour(contours):
+    recCon = [] # all rect (polygons re) rahega isme
+    for i in contours:
+        area = cv2.contourArea(i)
+        #print("area:",i,area)
+
+        if area > 50:
+            peri   = cv2.arcLength(i, True)
+            approx = cv2.approxPolyDP(i, 0.02*peri, True)
+            if len(approx) == 4:    #(rect poly)
+                recCon.append(i)
+            #print("connor :",len(approx),approx,area)
+
+    rectCon = sorted(recCon, key = cv2.contourArea, reverse = True)
+    return rectCon
+
+
+def getCornerPoints(contour):
+    peri   = cv2.arcLength(contour, True)
+    approx = cv2.approxPolyDP(contour, 0.02*peri, True)
+
+    return approx
+
+def reorderPoints(myPoints):
+    myPoints = myPoints.reshape((4,2))      # (4,1,2) --> (4,2)
+    add = myPoints.sum(1)                   # axis    --> 1
+    print(add)
+    myPointsNew = np.zeros((4,1,2), np.int32)
+
+    myPointsNew[0] = myPoints[np.argmin(add)]
+    myPointsNew[3] = myPoints[np.argmax(add)]
+
+    diff = np.diff(myPoints, axis = 1)
+    myPointsNew[1] = myPoints[np.argmin(diff)]
+    myPointsNew[2] = myPoints[np.argmax(diff)]
+
+    return myPointsNew
+
+def splitBoxes(img):
+    #split vertically ie get row 1
+    rows = np.vsplit(img, 5)
+    boxes = []
+
+    # cv2.imshow("row",rows)
+    for r in rows:
+        cols = np.hsplit(r, 5)
+
+        for box in cols:
+            boxes.append(box)
+            # cv2.imshow("i",box)
+
+    return boxes
+
+def showAnswers(img,myIndex,grading,ans,questions=5,choices=5):
+     secW = int(img.shape[1]/questions)
+     secH = int(img.shape[0]/choices)
+
+     for x in range(0,questions):
+         myAns= myIndex[x]
+         cX = (myAns * secW) + secW // 2
+         cY = (x * secH) + secH // 2
+         if grading[x]==1:
+            myColor = (0,255,0)
+            #cv2.rectangle(img,(myAns*secW,x*secH),((myAns*secW)+secW,(x*secH)+secH),myColor,cv2.FILLED)
+            cv2.circle(img,(cX,cY),20,myColor,cv2.FILLED)
+         else:
+            myColor = (0,0,255)
+            #cv2.rectangle(img, (myAns * secW, x * secH), ((myAns * secW) + secW, (x * secH) + secH), myColor, cv2.FILLED)
+            cv2.circle(img, (cX, cY), 20, myColor, cv2.FILLED)
+
+            # CORRECT ANSWER
+            myColor = (0, 255, 0)
+            correctAns = ans[x]
+            cv2.circle(img,((correctAns * secW)+secW//2, (x * secH)+secH//2),
+            10,myColor,cv2.FILLED)
+
+     return img    
+
+
